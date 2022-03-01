0c0126eb2d57b74a93c1ac08b1c36adbb6514b87
empty <a@a.aa>
2022-03-01 05:41:20 -0500

    Update main program
    - Added a delay to the killswitch (default 30 seconds) in the case that there is minor instability or user is changing VPN locations
    - Can now take an argument (an integer) for how long to wait before "flipping" the killswitch 
    - Upon hitting enter after the killswitch is triggered the network is reset and the NIC data is saved so the switch is reset 
    - The VPN does NOT need to be turned on prior to starting the killswitch and now it will wait before the VPN is enabled before arming the killswitch
    - Checks to see if there is internet access prior to re-enabling the internet connection (before arming the killswitch), also reports currently established IP address when this is checked

--- killswitch-windows/Program.cs
+++ killswitch-windows/Program.cs
@@ -7,6 +7,8 @@ using System.Drawing;
 using Console = Colorful.Console;
 using System.Net.NetworkInformation;
 using System.Diagnostics;
+using System.Net;
+using System.Web;
 
 namespace killswitch_windows
 {
@@ -24,28 +26,23 @@ namespace killswitch_windows
         static private string vpnNIC;
         static private Int32 nicChoice;
         static private bool isKill;
-   
+        static private int Time = 30;
         static void Main(string[] args)
         {
 
             Console.Write("==============================================================", Color.Blue);
             Console.WriteLine(""+assLogo, Color.Cyan);
-            Console.Write("==============================================================", Color.Blue);
-
+            Console.WriteLine("==============================================================", Color.Blue);
+            if (args.Length > 0)
+            {
+                Int32.TryParse(args[0],out Time);
+            }
             Start();
+
         }
 
         static void Start()
         {
-
-
-            // enable internet connection (it could have been disabled before calling this function)
-            ProcessStartInfo info = new ProcessStartInfo();
-            info.FileName = "ipconfig";
-            info.Arguments = "/renew"; // or /release if you want to disconnect
-            info.WindowStyle = ProcessWindowStyle.Hidden;
-            Process p = Process.Start(info);
-            p.WaitForExit();
 
 
             // get list of interfaces
@@ -55,29 +52,67 @@ namespace killswitch_windows
             // ask the user which interface the vpn runs on
             Console.WriteLine("\nSelect which network interface your vpn is running on.");
             //foreach (NetworkInterface n in adapters)
-            foreach(var x in adapters.Select((value, index) => new { value, index }))
+            foreach (var x in adapters.Select((value, index) => new { value, index }))
             {
                 Console.WriteLine("\t{0} - {1}", x.index, x.value.Name);
             }
             Console.Write(">");
             nicChoice = Convert.ToInt32(Console.ReadLine());
 
-
             // store the interface id to a member value so we can compate when we detect a network change 
             vpnNIC = adapters[nicChoice].Id;
 
 
-            // start listening for a network change
-            isKill = false;
-            NetworkChange.NetworkAddressChanged += new
-            NetworkAddressChangedEventHandler(AddressChangedCallback);
-            Console.Write("\n[STARTED]", Color.Green);
-            Console.Write(" killswitch is now listening for a change in your supplied interface.");
+            while (true) { 
+                try
+                {
+                    string externalIpString = new WebClient().DownloadString("http://icanhazip.com").Replace("\\r\\n", "").Replace("\\n", "").Trim();
 
-            // stop the program from terminating so we can keep the listener going
-            Console.ReadLine();
+                    var externalIp = IPAddress.Parse(externalIpString);
+
+                    Console.WriteLine("Network online with current IP as: " + externalIp.ToString());
+                }
+                catch
+                {
+                    Console.WriteLine("Network currently down. Bringing Online");
+                    // enable internet connection (it could have been disabled before calling this function)
+                    ProcessStartInfo info = new ProcessStartInfo();
+                    info.FileName = "ipconfig";
+                    info.Arguments = "/renew"; // or /release if you want to disconnect
+                    info.WindowStyle = ProcessWindowStyle.Hidden;
+                    Process p = Process.Start(info);
+                    p.WaitForExit();
+                }
 
 
+                //Wait until VPN is operational first.
+                if (adapters[nicChoice].OperationalStatus == OperationalStatus.Down)
+                {
+                    Console.WriteLine("Waiting for VPN to come online");
+
+                    while (adapters[nicChoice].OperationalStatus == OperationalStatus.Down)
+                    {
+
+                        Console.WriteLine("waiting 5 seconds");
+                        Console.WriteLine(adapters[nicChoice].OperationalStatus);
+                        System.Threading.Thread.Sleep(5000);
+                        adapters = NetworkInterface.GetAllNetworkInterfaces();
+                    }
+                    Console.WriteLine("VPN Online");
+                }
+
+                // start listening for a network change
+                isKill = false;
+                NetworkChange.NetworkAddressChanged += new
+                NetworkAddressChangedEventHandler(AddressChangedCallback);
+                Console.Write("\n[STARTED]", Color.Green);
+                Console.Write(" killswitch is now listening for a change in your supplied interface.");
+
+                // stop the program from terminating so we can keep the listener going
+                Console.ReadLine();
+
+                Console.Clear();
+            }
         }
 
         static void AddressChangedCallback(object sender, EventArgs e)
@@ -93,10 +128,22 @@ namespace killswitch_windows
                 Console.Write(" Checking if our vpn has been disabled.\n");
                 if (adapters[nicChoice].OperationalStatus == OperationalStatus.Down)
                 {
-                    isKill = true;
-                    KillNow();
-                    Console.Write("[INTERNET DISABLED]", Color.Red);
-                    Console.Write(" your vpn was dissconnected. (restart the kill switch and turn on your vpn before selecting the NIC)");
+                    System.Threading.Thread.Sleep(Time * 1000); //wait x seconds for the chance for a different vpn (default 30)
+                    adapters = NetworkInterface.GetAllNetworkInterfaces();
+                    if (adapters[nicChoice].OperationalStatus == OperationalStatus.Down) {
+                        isKill = true;
+                        Console.Beep(570, 100);
+                        Console.Beep(570, 100);
+                        Console.Beep(570, 100);
+                        Console.Beep(570, 100);
+                        KillNow();
+                        Console.Write("[INTERNET DISABLED]", Color.Red);
+                        Console.Write(" your vpn was disconnected. (press enter to restart the kill switch)");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Saved for now!");
+                    }
                 }
                 
             }
